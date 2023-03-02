e736f90770d89fc791044537f673ee8cca90ce4d
63070158 <63070158@kmitl.ac.th>
2023-03-02 16:40:30 +0700

    edit py

--- myscript.py
+++ myscript.py
@@ -1,4 +1,4 @@
-import argpars
+import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("name", help="the name to greet")
 parser.add_argument("--age", help="the age of the person being greeted", type=int)
